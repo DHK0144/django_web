1b50235bcecd68505db1aa207256e1f1112a9acd
DESKTOP-CNTTNUQ\HOME <dongha0144@naver.com>
2021-09-08 02:28:13 +0900

    Swagger update

--- movie/serializers.py
+++ movie/serializers.py
@@ -8,3 +8,21 @@ class MusicSerializer(serializers.ModelSerializer):
         model = Music
         fields = '__all__'  # __all__ 을 줄 경우, 모든 필드가 사용됨.
         # fields = ('id', 'created_at', 'title', 'category', 'star_rating',)  # req, res 시 사용되길 원하는 필드(컬럼)만 적어줘도 됨.
+
+
+class MusicBodySerializer(serializers.Serializer):
+    singer = serializers.CharField(help_text="가수명")
+    title = serializers.CharField(help_text="곡 제목")
+    category = serializers.ChoiceField(help_text="곡 범주", choices=('JPOP', 'POP', 'CLASSIC', 'ETC'))
+    star_rating = serializers.IntegerField(help_text="1~3 이내 지정 가능. 숫자가 클수록 좋아하는 곡")
+
+
+class MusicQuerySerializer(serializers.Serializer):
+    title = serializers.CharField(help_text="곡 제목으로 검색", required=False)
+    star_rating = serializers.ChoiceField(help_text="곡 선호도로 검색", choices=(1, 2, 3), required=False)
+    singer = serializers.CharField(help_text="가수명으로 검색", required=True)
+    category_1 = serializers.ChoiceField(help_text="카테고리로 검색", choices=('JPOP', 'POP', 'CLASSIC', 'ETC'), required=False)
+    category_2 = serializers.ChoiceField(help_text="카테고리로 검색", choices=('JPOP', 'POP', 'CLASSIC', 'ETC'), required=False)
+    category_3 = serializers.ChoiceField(help_text="카테고리로 검색", choices=('JPOP', 'POP', 'CLASSIC', 'ETC'), required=False)
+    category_4 = serializers.ChoiceField(help_text="카테고리로 검색", choices=('JPOP', 'POP', 'CLASSIC', 'ETC'), required=False)
+    created_at = serializers.DateTimeField(help_text="입력한 날짜를 기준으로 그 이전에 추가된 곡들을 검색", required=False)
--- movie/views.py
+++ movie/views.py
@@ -1,16 +1,26 @@
+from drf_yasg import openapi
 from rest_framework import status, viewsets, mixins
 from rest_framework.response import Response
 from django.views import View
 from django.http import Http404
+from drf_yasg.utils import swagger_auto_schema, no_body
 
 from .models import Music
-from .serializers import MusicSerializer
+from .serializers import MusicSerializer, MusicBodySerializer, MusicQuerySerializer
 
 # Create your views here.
 
+# https://velog.io/@lu_at_log/drf-yasg-and-swagger 참조
 
 class MusicViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, View):
     serializer_class = MusicSerializer   # 이 클래스형 view 에서 사용할 시리얼라이저를 선언
+
+    # get_queryset에 데코레이터를 붙이면 인식을 못 하기 때문에 list를 상속 받아서 구현했다.
+    # get_queryset은 list() 에서 불리는 함수에 불과하다.
+    # 실제 response 하는 메소드는 list 이기 때문에 list를 상속받아서 데코레이터를 붙여준다.
+    @swagger_auto_schema(query_serializer=MusicQuerySerializer)
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def get_queryset(self):
         conditions = {
@@ -29,6 +39,9 @@ class MusicViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, View):
 
         return musics
 
+    # @swagger_auto_schema(request_body=no_body) # body가 필요없을 경우
+    @swagger_auto_schema(request_body=MusicBodySerializer, # body용 serializer 새로 만듬
+                         manual_parameters=[openapi.Parameter('header_test', openapi.IN_HEADER, description="a header for  test", type=openapi.TYPE_STRING)])
     def add(self, request):
         musics = Music.objects.filter(**request.data)
         if musics.exists():
